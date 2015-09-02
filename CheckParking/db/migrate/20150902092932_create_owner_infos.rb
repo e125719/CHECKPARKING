74545358b1689321e29513e660fd924cbc6ec077
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-09-02 19:27:28 +0900

    modify 'db/migrate/20150902092932_create_owner_infos.rb' to create a table 'owner_infos'

--- CheckParking/db/migrate/20150902092932_create_owner_infos.rb
+++ CheckParking/db/migrate/20150902092932_create_owner_infos.rb
@@ -2,6 +2,10 @@ class CreateOwnerInfos < ActiveRecord::Migration
   def change
     create_table :owner_infos do |t|
 
+      t.string :owner_name
+      t.string :owner_dep
+      t.string :owner_attr
+
       t.timestamps null: false
     end
   end
