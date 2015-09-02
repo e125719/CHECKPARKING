df6c0b56c7c8d830fe919d35803f9b54456fbd62
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-09-02 19:21:56 +0900

    modify 'db/migrate/20150902063258_create_car_numbers.rb' to create a table 'car_numbers'

--- CheckParking/db/migrate/20150902063258_create_car_numbers.rb
+++ CheckParking/db/migrate/20150902063258_create_car_numbers.rb
@@ -2,6 +2,9 @@ class CreateCarNumbers < ActiveRecord::Migration
   def change
     create_table :car_numbers do |t|
 
+      t.integer :car_num
+      t.string :owner_name
+
       t.timestamps null: false
     end
   end
