67a0a2d7e4d6364b801b2ac442459cec8b773a27
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-09-14 15:36:44 +0900

    seed dummy datas into table 'car_info'

--- CheckParking/db/seeds.rb
+++ CheckParking/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+CSV.foreach('./db/seed.csv') do |row|
+	CarInfo.create(:number => row[0], :owner => row[1])
+end
